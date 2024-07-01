b202538c3e2c8d707f8309fdb03926afbb85830f
Eileen Yu <eileenylj@gmail.com>
2024-07-01 15:18:17 -0700

    check appid ready for pubsub

--- pubsub/java/entrypoint.sh
+++ pubsub/java/entrypoint.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
+check_appid_status() {
+    local appid_name=$1
+    local max_attempts=5
+    local attempt=1
+
+    while [ $attempt -le $max_attempts ]; do
+        status=$(diagrid appid get $appid_name | grep 'Status:' | awk '{print $2}')
+        echo "Attempt $attempt: Current status of $appid_name: $status"
+        if [ "$status" == "ready" ]; then
+            break
+        fi
+        if [ $attempt -eq $max_attempts ]; then
+            echo "Max attempts reached. $appid_name is not ready."
+            exit 1
+        fi
+        echo "Waiting for 10 seconds..."
+        sleep 10
+        attempt=$((attempt + 1))
+    done
+}
+
 # Create a project
 echo "Creating project..."
 diagrid project create pubsub-java-project-container --deploy-managed-pubsub
@@ -17,8 +38,9 @@ echo "Creating Subscription..."
 diagrid subscription create pubsub-subscriber --connection pubsub --topic orders --route /pubsub/neworders --scopes subscriber
 
 
-echo "Waiting for 30 seconds..."
-sleep 30
+echo "Wating for AppIDs get ready..."
+check_appid_status publisher
+check_appid_status subscriber
 
 # Scaffold dev config file 
 echo "Scaffolding dev config file..."
