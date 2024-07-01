c1e9c61bf52bb58f52010b55884d8f5b60c9a767
Eileen Yu <eileenylj@gmail.com>
2024-07-01 15:13:05 -0700

    check appid ready for kv

--- keyvalue/csharp/entrypoint.sh
+++ keyvalue/csharp/entrypoint.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
+check_appid_status() {
+    local appid_name=$1
+    local max_attempts=5
+    local attempt=1
+
+    while [ $attempt -le $max_attempts ]; do
+        status=$(diagrid appid get $appid_name | grep 'Status:' | awk '{print $2}')
+        echo "Attempt $attempt: Current status of $appid_name: $status"
+        if [ "$status" == "ready" ]; then
+            break
+        fi
+        if [ $attempt -eq $max_attempts ]; then
+            echo "Max attempts reached. $appid_name is not ready."
+            exit 1
+        fi
+        echo "Waiting for 10 seconds..."
+        sleep 10
+        attempt=$((attempt + 1))
+    done
+}
 # Create a project
 echo "Creating project..."
 diagrid project create kv-csharp-project-container --deploy-managed-kv
@@ -11,8 +31,8 @@ diagrid project use kv-csharp-project-container
 echo "Creating AppID..."
 diagrid appid create orderapp
 
-echo "Waiting for 30 seconds..."
-sleep 30
+echo "Wating for AppID get ready..."
+check_appid_status orderapp
 
 # Connect the application to Catalyst 
 echo "Starting the application..."
