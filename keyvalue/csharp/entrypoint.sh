d8b01967452757cd327e6d77c9f24855c15f9e01
Eileen Yu <eileenylj@gmail.com>
2024-07-09 15:42:38 -0700

    csharp kv quickstart rework

--- keyvalue/csharp/entrypoint.sh
+++ keyvalue/csharp/entrypoint.sh
@@ -1,39 +1,12 @@
 #!/bin/bash
-check_appid_status() {
-    local appid_name=$1
-    local max_attempts=5
-    local attempt=1
+chmod +x scaffold.py
 
-    while [ $attempt -le $max_attempts ]; do
-        status=$(diagrid appid get $appid_name | grep 'Status:' | awk '{print $2}')
-        echo "Attempt $attempt: Current status of $appid_name: $status"
-        if echo "$status" | grep -q "ready"; then
-            break
-        fi
-        if [ $attempt -eq $max_attempts ]; then
-            echo "Max attempts reached. $appid_name is not ready."
-            exit 1
-        fi
-        echo "Waiting for 10 seconds..."
-        sleep 10
-        attempt=$((attempt + 1))
-    done
-}
-# Create a project
-echo "Creating project..."
-diagrid project create kv-csharp-project-container --deploy-managed-kv
+# Create a project and set up the environment
+python3 run.py --project-name kv-csharp-project-container --config-file "$CONFIG_FILE" --is-container
 
-# Set this project as the default project
-echo "Setting default project..."
-diagrid project use kv-csharp-project-container
+# Install deps
+dotnet build
+echo "Dependencies installed."
 
-# Create an AppID
-echo "Creating App ID orderapp..."
-diagrid appid create orderapp
-
-echo "Waiting for App ID orderapp to get ready..."
-check_appid_status orderapp
-
-# Connect the application to Catalyst 
-echo "Starting the application..."
-diagrid dev start --app-id orderapp --env PORT=5001 "dotnet run --urls=http://0.0.0.0:5001"
+# Start the application
+diagrid dev start -f "$CONFIG_FILE"
---
+++ keyvalue/csharp/scaffold.py
@@ -0,0 +1,20 @@
+import os
+import yaml
+
+config_file = os.getenv('CONFIG_FILE', 'dev-kv-csharp-project-local.yaml')
+with open(config_file, 'r') as file:
+    config_data = yaml.load(file, Loader=yaml.FullLoader)
+
+for app in config_data['apps']:
+    if app['appId'] == 'orderapp':
+        app['workDir'] = '.'
+        app['command'] = ['dotnet', 'run']
+        app['env']['PORT'] = 5001
+        app['env']['ASPNETCORE_URLS'] = 'http://localhost:5001'
+
+with open(config_file, 'w') as file:
+    yaml.safe_dump(config_data, file, default_flow_style=False, sort_keys=False)
+
+print("YAML file has been updated.")
+
+
