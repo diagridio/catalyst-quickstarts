4883acf9b7a6173c81dabc5c9385d1a257c57b4e
Eileen Yu <eileenylj@gmail.com>
2024-07-12 14:59:45 -0700

    cli support

---
+++ workflow/python/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Create a project and set up the environment
+python3 run.py --project-name workflow-python-project-container --config-file "$CONFIG_FILE" --is-container
+
+# Install deps
+python3 -m venv diagrid-venv
+source diagrid-venv/bin/activate
+pip install --upgrade pip
+pip install certifi
+pip install --no-cache-dir -r requirements.txt
+source diagrid-venv/bin/activate
+echo "Dependencies installed."
+
+# Start the application
+diagrid dev start -f "$CONFIG_FILE"
---
+++ workflow/python/scaffold.py
@@ -0,0 +1,17 @@
+import os
+import yaml
+
+config_file = os.getenv('CONFIG_FILE', 'dev-workflow-python-project-local.yaml')
+with open(config_file, 'r') as file:
+    config_data = yaml.load(file, Loader=yaml.FullLoader)
+
+for app in config_data['apps']:
+    if app['appId'] == 'workflow-app':
+        app['workDir'] = '.'
+        app['command'] = ['uvicorn', 'main:app', '--port', '5001']
+        app['env']['PORT'] = 5001
+
+with open(config_file, 'w') as file:
+    yaml.safe_dump(config_data, file, default_flow_style=False, sort_keys=False)
+
+print("YAML file has been updated.")
