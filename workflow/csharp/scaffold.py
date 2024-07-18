5e5046fadee3a803353328223ee9c8d37bd5fb98
Eileen Yu <eileenylj@gmail.com>
2024-07-18 14:11:51 -0700

    fix csharp container host binding

--- workflow/csharp/scaffold.py
+++ workflow/csharp/scaffold.py
@@ -10,7 +10,7 @@ for app in config_data['apps']:
         app['workDir'] = '.'
         app['command'] = ['dotnet', 'run']
         app['env']['PORT'] = 5001
-        app['env']['ASPNETCORE_URLS'] = 'http://localhost:5001'
+        app['env']['ASPNETCORE_URLS'] = 'http://0.0.0.0:5001'
 
 with open(config_file, 'w') as file:
     yaml.safe_dump(config_data, file, default_flow_style=False, sort_keys=False)
