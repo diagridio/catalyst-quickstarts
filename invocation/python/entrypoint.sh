d12532d2158fe716e64423717c6dbd6f972364e3
Eileen Yu <eileenylj@gmail.com>
2024-07-18 16:17:30 -0700

    feat: python service invocation

---
+++ invocation/python/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Create a project and set up the environment
+python3 run.py --project-name invoke-python-project-container --config-file "$CONFIG_FILE" --is-container
+
+# Install deps
+python3 -m venv venv
+source venv/bin/activate
+pip install --upgrade pip
+pip install certifi
+pip install --no-cache-dir -r requirements.txt
+echo "Dependencies installed."
+
+# Start the application
+diagrid dev start -f "$CONFIG_FILE"
---
+++ invocation/python/main.py
@@ -0,0 +1,50 @@
+from dapr.clients import DaprClient
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+import logging
+import grpc
+import requests
+import os
+
+app = FastAPI()
+
+logging.basicConfig(level=logging.INFO)
+
+
+class Order(BaseModel):
+    orderId: int
+
+# Set up required inputs for http client to perform service invocation
+base_url = os.getenv('DAPR_HTTP_ENDPOINT', 'http://localhost')
+dapr_api_token = os.getenv('DAPR_API_TOKEN', '')
+invoke_target_appid = os.getenv('INVOKE_APPID', 'target')
+
+@app.post('/invoke/orders')
+async def send_order(order: Order):
+    headers = {'dapr-app-id': invoke_target_appid, 'dapr-api-token': dapr_api_token,
+               'content-type': 'application/json'}
+    try:
+        result = requests.post(
+            url='%s/invoke/neworders' % (base_url),
+            data=order.model_dump_json(),
+            headers=headers
+        )
+
+        if result.ok:
+            logging.info('Invocation successful with status code: %s' %
+                         result.status_code)
+            return str(order)
+        else:
+            logging.error(
+                'Error occurred while invoking App ID: %s' % result.reason)
+            raise HTTPException(status_code=500, detail=result.reason)
+
+    except grpc.RpcError as err:
+        logging.error(f"ErrorCode={err.code()}")
+        raise HTTPException(status_code=500, detail=err.details())
+
+
+@app.post('/invoke/neworders')
+def receive_order(order: Order):
+    logging.info('Request received : ' + str(order))
+    return str(order)
