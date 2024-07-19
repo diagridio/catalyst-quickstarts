7c15d281bdc018893f6ee44f0125b6c3f5f7bad9
Eileen Yu <eileenylj@gmail.com>
2024-07-19 13:11:47 -0700

    java invocation sample code

---
+++ invocation/java/src/main/java/com/service/controller/Controller.java
@@ -0,0 +1,87 @@
+package com.service.controller;
+
+import io.dapr.client.DaprClient;
+import io.dapr.client.DaprClientBuilder;
+import io.dapr.client.domain.CloudEvent;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+
+import io.dapr.client.domain.State;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.json.JSONObject;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Mono;
+import javax.annotation.PostConstruct;
+
+@RestController
+public class Controller {
+  private static final Logger logger = LoggerFactory.getLogger(Controller.class);
+  private DaprClient client;
+  private HttpClient httpClient;
+
+  private static final String DAPR_HTTP_ENDPOINT = System.getenv().getOrDefault("DAPR_HTTP_ENDPOINT",
+      "http://localhost");
+  private static final String DAPR_API_TOKEN = System.getenv().getOrDefault("DAPR_API_TOKEN", "");
+  private static final String INVOKE_TARGET_APPID = System.getenv().getOrDefault("INVOKE_APPID", "target");
+
+  @PostConstruct
+  public void init() {
+    client = new DaprClientBuilder().build();
+    httpClient = HttpClient.newBuilder()
+        .version(HttpClient.Version.HTTP_2)
+        .connectTimeout(Duration.ofSeconds(10))
+        .build();
+  }
+
+  // Invoke another service
+  @PostMapping(path = "/invoke/orders", consumes = MediaType.ALL_VALUE)
+  public Mono<ResponseEntity> request(@RequestBody(required = true) Order order) {
+    return Mono.fromSupplier(() -> {
+      try {
+        JSONObject obj = new JSONObject();
+        obj.put("orderId", order.getOrderId());
+
+        HttpRequest request = HttpRequest.newBuilder()
+            .POST(HttpRequest.BodyPublishers.ofString(obj.toString()))
+            .uri(URI.create(DAPR_HTTP_ENDPOINT + "/invoke/neworders"))
+            .header("dapr-api-token", DAPR_API_TOKEN)
+            .header("Content-Type", "application/json")
+            .header("dapr-app-id", INVOKE_TARGET_APPID)
+            .build();
+
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+        JSONObject jsonObject = new JSONObject(response.body());
+        logger.info("Invoke Successful. Reply received: " + jsonObject.getInt("orderId"));
+        return ResponseEntity.ok("SUCCESS");
+      } catch (Exception e) {
+        logger.error("Error occurred while invoking App ID. " + e);
+        throw new RuntimeException(e);
+      }
+    });
+  }
+
+  // Service to be invoked
+  @PostMapping(path = "/invoke/neworders", consumes = MediaType.ALL_VALUE)
+  public ResponseEntity<Order> reply(@RequestBody Order order) {
+    System.out.println("Request received : " + order.getOrderId());
+    return ResponseEntity.ok(order);
+  }
+}
+
+@Getter
+@Setter
+@ToString
+class Order {
+  private int orderId;
+}
