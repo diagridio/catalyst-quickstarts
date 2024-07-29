da27197c64d3c0d27be70b8283048733a8372562
Eileen Yu <eileenylj@gmail.com>
2024-07-29 10:19:11 -0700

    allow pass project name to kv python

--- keyvalue/python/entrypoint.sh
+++ keyvalue/python/entrypoint.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
+if [ -n "$QUICKSTART_PROJECT_NAME" ]; then
+  export CONFIG_FILE="dev-$QUICKSTART_PROJECT_NAME.yaml"
+else
+  echo "Error: QUICKSTART_PROJECT_NAME is not set."
+  exit 1
+fi
+
+echo "Using config file: $CONFIG_FILE"
+
+if [ -n "$QUICKSTART_PROJECT_NAME" ]; then
+  export PROJECT_NAME="$QUICKSTART_PROJECT_NAME"
+else
+  echo "Error: QUICKSTART_PROJECT_NAME is not set."
+  exit 1
+fi
+
+echo "Using project name: $QUICKSTART_PROJECT_NAME"
+
 # Create a project and set up the environment
-python3 run.py --project-name kv-python-project-container --config-file "$CONFIG_FILE" --is-container
+python3 run.py --project-name "$PROJECT_NAME" --config-file "$CONFIG_FILE" --is-container
 
 # Install deps
 python3 -m venv venv
