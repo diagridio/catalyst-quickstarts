b7c0d3d8dda0fc89846043a7718373247805cacc
Eileen Yu <eileenylj@gmail.com>
2024-08-02 10:33:05 -0700

    cleanup

--- pubsub/csharp/entrypoint.sh
+++ pubsub/csharp/entrypoint.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
 if [ -n "$QUICKSTART_PROJECT_NAME" ]; then
-  export CONFIG_FILE="dev-$QUICKSTART_PROJECT_NAME.yaml"
-else
-  echo "Error: QUICKSTART_PROJECT_NAME is not set."
-  exit 1
-fi
-
-echo "Using config file: $CONFIG_FILE"
-
-if [ -n "$QUICKSTART_PROJECT_NAME" ]; then
   export PROJECT_NAME="$QUICKSTART_PROJECT_NAME"
+  echo "Using project name: $QUICKSTART_PROJECT_NAME"
+  export CONFIG_FILE="dev-$QUICKSTART_PROJECT_NAME.yaml"
+  echo "Using config file: $CONFIG_FILE"
 else
   echo "Error: QUICKSTART_PROJECT_NAME is not set."
   exit 1
 fi
-
-echo "Using project name: $QUICKSTART_PROJECT_NAME"
 
 # Create a project and set up the environment
 python3 run.py --project-name "$PROJECT_NAME" --config-file "$CONFIG_FILE" --is-container
