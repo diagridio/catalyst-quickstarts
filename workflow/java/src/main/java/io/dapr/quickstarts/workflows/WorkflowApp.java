ea13d04384ee60f5c2e47387cf17a513f5a55a89
Eileen Yu <eileenylj@gmail.com>
2025-05-22 15:00:18 -0700

    drop workflowruntime

--- workflow/java/src/main/java/io/dapr/quickstarts/workflows/WorkflowApp.java
+++ workflow/java/src/main/java/io/dapr/quickstarts/workflows/WorkflowApp.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import io.dapr.workflows.client.DaprWorkflowClient;
 import io.dapr.workflows.client.WorkflowInstanceStatus;
-import io.dapr.workflows.runtime.WorkflowRuntime;
+import io.dapr.spring.workflows.config.EnableDaprWorkflows;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,10 +19,8 @@ import io.dapr.quickstarts.workflows.models.*;
 
 @SpringBootApplication
 @RestController
+@EnableDaprWorkflows
 public class WorkflowApp {
-
-  @Autowired
-  private WorkflowRuntime workflowRuntime;
 
   @Autowired
   private DaprWorkflowClient workflowClient;
@@ -37,11 +35,6 @@ public class WorkflowApp {
   public ResponseEntity<String> startWorkflow(@RequestBody OrderPayload order) {
     logger.info("Received request to start workflow for item: {} with quantity: {}", order.getItemName(),
         order.getQuantity());
-
-    if (workflowRuntime == null) {
-      logger.error("Workflow runtime is not initialized");
-      throw new IllegalStateException("Workflow runtime is not initialized");
-    }
 
     try {
       String instanceId = workflowClient.scheduleNewWorkflow(OrderProcessingWorkflow.class, order);
